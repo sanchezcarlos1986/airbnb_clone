8f9e207c1b582dc50316d9ec4eb54f164a2a0adb
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2021-01-02 00:38:45 -0300

    feat(components): new Post component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { HomeScreen } from "~/screens/";
+import { Post } from "~/components/";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <Post />
     </View>
   );
 }
