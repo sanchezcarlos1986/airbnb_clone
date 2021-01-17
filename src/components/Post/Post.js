370a67aee4f19395d328686a718b9d2ecfd0882d
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2021-01-17 00:26:31 -0300

    fix(components): scrollview in post when detailed

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Text, Pressable } from "react-native";
+import { Image, Text, Pressable, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { styles } from "./Post.styles";
 
@@ -9,34 +9,39 @@ const Post = ({ post, detailed }) => {
     navigation.navigate("PostScreen", { postId: post.id });
 
   return (
-    <Pressable style={styles.container} onPress={goToPostPage}>
-      {/* Image */}
-      <Image style={styles.image} source={{ uri: post.image }} />
+    <ScrollView>
+      <Pressable
+        style={styles.container}
+        onPress={detailed ? null : goToPostPage}
+      >
+        {/* Image */}
+        <Image style={styles.image} source={{ uri: post.image }} />
 
-      {/* Beds and Bedroom */}
-      <Text style={styles.bedrooms}>
-        {post.bed} {post.bed === 1 ? "bed" : "beds"} · {post.bedroom}{" "}
-        {post.bedroom === 1 ? "bedroom" : "bedrooms"}
-      </Text>
+        {/* Beds and Bedroom */}
+        <Text style={styles.bedrooms}>
+          {post.bed} {post.bed === 1 ? "bed" : "beds"} · {post.bedroom}{" "}
+          {post.bedroom === 1 ? "bedroom" : "bedrooms"}
+        </Text>
 
-      {/* Type and Description */}
-      <Text style={styles.description} numberOfLines={2}>
-        {`${post.type}. ${post.title}`}
-      </Text>
+        {/* Type and Description */}
+        <Text style={styles.description} numberOfLines={2}>
+          {`${post.type}. ${post.title}`}
+        </Text>
 
-      {/* Prices */}
-      <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>{`$${post.oldPrice}`}</Text>{" "}
-        <Text style={styles.price}>{`$${post.newPrice}`}</Text> / night
-      </Text>
+        {/* Prices */}
+        <Text style={styles.prices}>
+          <Text style={styles.oldPrice}>{`$${post.oldPrice}`}</Text>{" "}
+          <Text style={styles.price}>{`$${post.newPrice}`}</Text> / night
+        </Text>
 
-      {/* Total price */}
-      <Text style={styles.totalPrice}>{`$${post.totalPrice} total`}</Text>
+        {/* Total price */}
+        <Text style={styles.totalPrice}>{`$${post.totalPrice} total`}</Text>
 
-      {post?.description && Boolean(detailed) ? (
-        <Text style={styles.longDescription}>{post.description}</Text>
-      ) : null}
-    </Pressable>
+        {post?.description && Boolean(detailed) ? (
+          <Text style={styles.longDescription}>{post.description}</Text>
+        ) : null}
+      </Pressable>
+    </ScrollView>
   );
 };
 
